f27c25c92ec5e6ccd0235a48888f524f5bfe6c60
unknown <1911096116@qq.com>
2018-10-30 08:40:17 +0800

    szh-2

--- src/components/find/index.js
+++ src/components/find/index.js
@@ -1,11 +1,23 @@
 import React,{Component} from "react"
 import {connect} from "react-redux"
+import {NavLink} from "react-router-dom"
+import axios from "axios"
 class Find extends Component{
 	render(){
 		return(<div>
-				Find
+				<ul>
+					<li><NavLink to="/find/news"></NavLink></li>
+					<li><NavLink to="/find/review"></NavLink></li>
+					<li><NavLink to="/find/toplist"></NavLink></li>
+					<li><NavLink to="/find/trailer"></NavLink></li>
+				</ul>
 				{this.props.children}
 			</div>)
+	}
+	componentDidMount(){
+		axios.get('/Service/callback.mi/movie/Detail.api?movieId=219640&locationId=290&t=201810308333668503').then(res=>{
+			console.log(res.data)
+		})
 	}
 }
 export default connect()(Find)
---
+++ src/setupProxy.js
@@ -0,0 +1,8 @@
+var proxy=require("http-proxy-middleware")
+module.exports=function(app){
+	app.use(proxy("/Service",{
+		target:"http://m.mtime.cn",
+		host:"m.mtime.cn",
+		changeOrigin:true
+	}))
+}
