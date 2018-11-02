d8e498f97f9c070f5c06aa8b61ba632488b7e3cc
unknown <1911096116@qq.com>
2018-11-02 16:26:21 +0800

    szh-5

--- src/components/finddetail/toplistdetail/index.js
+++ src/components/finddetail/toplistdetail/index.js
@@ -10,30 +10,30 @@ class Toplistdetail extends Component{
 	}
 	render(){
 		return(<div id="toplistdetail"> {/**/}
-			{this.state.topdata?<div className="all">
-				<div className="head">
+			{this.state.topdata?<div className="toplistall">
+				<div className="toplisthead">
 					<h2>{this.state.topdata.topList.name}</h2>
 					<p>{this.state.topdata.topList.summary}</p>
 				</div>
 
-				<ul className="list">
-				{this.state.topdata.movies.map((item,index)=><li key={item.id}>
+				<ul className="toplist">
+				{this.state.topdata.movies.map((item,index)=><li key={item.id} onClick={this.toContent.bind(this,item.id)} className="toplistli">
 					<span className="indexnum">{index}</span>
-					<div className="right">
-						<div className="upper"> 
+					<div className="topright">
+						<div className="toplistupper"> 
 							<img src={item.posterUrl} alt=""/>
-							<div className="text">
+							<div className="toplisttext">
 								<h3>{item.name}({item.decade})  <span>{item.rating}</span> <br/>
 									<span>{item.nameEn}</span>
 								</h3>
-								<p className="actor">
+								<p className="toplistactor">
 									<span>导演：&nbsp;{item.director}</span> <br/>
 									<span>主演：&nbsp;{item.actor} {item.actor2}</span> <br/>
 									<span>{item.releaseDate}&nbsp;{item.releaseLocation}</span>
 								</p>
 							</div>
 						</div>
-						<p className="under">{item.remark}</p>
+						<p className="toplistunder">{item.remark}</p>
 					</div>
 					</li>)
 				}
@@ -49,6 +49,11 @@ class Toplistdetail extends Component{
 				topdata:res.data
 			})
 		})
+	}
+	toContent(i){
+		console.log(i)
+		window.location.href=`http://movie.wap.mtime.com/${i}/`
+		// this.props.history.push(`/finddetail/detail/${i}`)
 	}
 }
 export default Toplistdetail
--- src/components/finddetail/toplistdetail/toplistcontent/index.js
+++ src/components/finddetail/toplistdetail/toplistcontent/index.js
@@ -1,9 +1,91 @@
 import React,{Component} from "react"
+import axios from "axios"
+import "../../../find/toplist/iconfont1/iconfont.css"
+import { Drawer, Button } from 'antd';
+import 'antd/dist/antd.css'
+import "./index.scss"
 class Toplistcontent extends Component{
+	componentWillMount(){
+		
+	}
+	constructor(){
+		super();
+		this.state={
+			visible: false,
+			topdetail:null
+		}
+	}
 	render(){
-		return(<div>
-			Toplistcontent
+		var divobj={width:"33%",float:"left",textAlign:"center"}
+		var imgobj={width:"40%"}
+		var obj={background:"rgba(0,0,0,.5)"}
+		return(<div id="topdetail">
+			{this.state.topdetail?<div>
+				<div id="topdetailhead">
+					<div id="topdetailnav">
+						<i className="iconfont icon-fanhui-copy-copy" onClick={this.returnPrev.bind(this)}></i>
+						<i className="iconfont icon-fenxiang" type="primary" onClick={this.showDrawer}></i>
+					</div>
+					
+				</div>
+			</div>:null}
+			Toplistcontent---{this.props.match.params.szh}
+			<Drawer 
+	          title="分享"
+	          closable={true}
+	          onClose={this.onClose}
+	          visible={this.state.visible}
+	          placement="bottom"
+	          height="160px"
+	          maskStyle={obj}
+	        >
+	         <div style={divobj}>
+	         	<img src="/shareimg/share06.png" alt="" style={imgobj}/>
+	         	<p>新浪微博</p>
+	         </div>
+	         <div style={divobj}>
+	         	<img src="/shareimg/share09.png" alt="" style={imgobj}/>
+	         	<p>QQ空间</p>
+	         </div>
+	         <div style={divobj}>
+	         	<img src="/shareimg/share07.png" alt="" style={imgobj}/>
+	         	<p>时光网</p>
+	         </div>
+	        </Drawer>
 			</div>)
 	}
+	componentDidMount(){
+		document.querySelector("#finddetail #finddetailnav").style.display="none"
+		axios.get(`/Service/callback.mi/movie/Detail.api?movieId=${this.props.match.params.szh}&locationId=292&t=201811215952916`).then(res=>{
+			console.log(res.data)
+			this.setState({
+				topdetail:res.data
+			})
+		})
+	}
+	componentWillUnmount(){
+		document.querySelector("#finddetail #finddetailnav").style.display="flex"
+	}
+	returnPrev(){
+		let urlStr=window.location.hash
+		let urlStart=window.location.hash.indexOf("finddetail")+11
+		let urlEnd=window.location.hash.lastIndexOf("/")
+		let secondRouter=urlStr.slice(urlStart,urlEnd)
+		console.log(urlStr)
+		console.log(secondRouter)
+		this.props.history.push("/find/"+secondRouter)
+	}
+	showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+    console.log("showDrawer")
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 }
 export default Toplistcontent
